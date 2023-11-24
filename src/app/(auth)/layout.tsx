aa6dfee61c8c102ec57b723bafbb76771972253d
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-11-24 09:07:19 -0300

    create layout

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,8 +1,27 @@
+import Image from "next/image";
 import { ReactNode } from "react";
+import backgroundImg from "../../../public/login_background.jpg";
+import logo from "../../../public/netflix_logo.svg";
 
 export default function AuthLayout({ children }: { children: ReactNode }) {
   return (
     <div className="relative flex h-screen w-screen flex-col bg-black md:items-center md:justify-center md:bg-transparent">
+      <Image
+        src={backgroundImg}
+        alt="bkgimg"
+        className="hidden sm:flex sm:object-cover -z-10 brightness-50"
+        priority
+        fill
+      />
+
+      <Image
+        src={logo}
+        alt="logo"
+        className="absolute left-4 top-4 object-contain md:left-10 md:top-6"
+        width={120}
+        height={120}
+        priority
+      />
       {children}
     </div>
   );

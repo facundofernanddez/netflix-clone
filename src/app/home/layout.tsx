be2d22e96400ae8f9ab997ca3a01d365411d5daf
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-11-30 11:46:29 -0300

    add navbar into layout

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -1,3 +1,4 @@
+import Navbar from "@/components/Navbar";
 import { authOptions } from "@/utils/auth";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
@@ -11,5 +12,12 @@ export default async function HomeLayout({
   const session = await getServerSession(authOptions);
 
   if (!session) redirect("/login");
-  return <>{children}</>;
+  return (
+    <>
+      <Navbar />
+      <main className="w-full max-w-7xl max-auto sm:px-6 lg:px-8">
+        {children}
+      </main>
+    </>
+  );
 }
